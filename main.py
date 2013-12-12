e73f45ef139e141a5acd5bee114838ae14606b88
adaam <adam.cws@gmail.com>
2013-12-12 14:22:34 +0800

    Create this project

---
+++ main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name: check_password
+# Purpose: Make sure username and password are correct
+#
+# Author:      adaam
+#
+# Created:     12/12/2013
+# Copyright:   (c) adaam 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+def chk_pw(method,user,passwd):
+    '''
+    chkpw(method,username,password)
+    '''
+    pass
+    return 1
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
